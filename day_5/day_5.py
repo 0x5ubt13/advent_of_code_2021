a3abe00727bd36cdee772e4248c1cc6f2d25cbc6
0x5ubt13 <5ubt13@protonmail.com>
2021-12-06 17:31:31 +0000

    sync

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -5,48 +5,89 @@ def get_input(filename):
         return [line.strip() for line in f]
 
 def draw_map():
-    m = {}
-    coords = [(x, y) for x in range(10) for y in range(10)]
+    diagram = {}
+    coords = [(x, y) for x in range(1000) for y in range(1000)]
     for coord in coords:
-        m 
+        diagram[coord] = 0
+    
+    return diagram
 
-def part_1(m, data):
-    for i in data:
-        ins = i.split(' -> ')
+def part_1(diagram, data):
+    for coords in data:
+        ins = coords.split(' -> ')
         initial = ins[0].split(',')
         final = ins[1].split(',')
-        print(f"initial: x = {initial[0]}, y = {initial[1]} , final: x = {final[0]}, y = {final[1]}")
-        print(f"Initial: {ins[0]}, final: {ins[1]}")
+        # print(f"initial: x = {initial[0]}, y = {initial[1]} , final: x = {final[0]}, y = {final[1]}")
+        # print(f"Initial: {ins[0]}, final: {ins[1]}")
         initial_x, initial_y, final_x, final_y = int(initial[0]), int(initial[1]), int(final[0]), int(final[1])
         
-        print(m)
+        if initial_x == final_x or initial_y == final_y:
+            if initial_x == final_x:
+                if initial_y < final_y:
+                    for coord in range(initial_y, final_y +1):
+                        diagram[(initial_x, coord)] += 1
+                        # print(f"diagram[(x: {initial_x}, y: {coord})] += 1")
+                elif initial_y > final_y:
+                    for coord in range(initial_y, final_y -1, -1):
+                        diagram[(initial_x, coord)] += 1
+                        # print(f"diagram[(x: {initial_x}, y: {coord})] += 1")
+            elif initial_y == final_y:
+                if initial_x < final_x:
+                    for coord in range(initial_x, final_x +1):
+                        diagram[(coord, initial_y)] += 1
+                        # print(f"diagram[(x: {coord}, y: {initial_y})] += 1")
+                elif initial_x > final_x:
+                    for coord in range(initial_x, final_x -1, -1):
+                        diagram[(coord, initial_y)] += 1
+                        # print(f"diagram[(x: {coord}, y: {initial_y})] += 1")
+        # Part 2
+        elif initial_x != final_x and initial_y != final_y:
+            if initial_y < final_y:
+                if initial_x < final_x:
+                    for coord in range(initial_x, final_x +1):
+                        if initial_x < initial_y:
+                            diagram[(coord, (initial_y - initial_x) + coord)] += 1
+                            # print(f"diagram[(x: {coord}, y: {(initial_y - initial_x) + coord})] += 1")
+                        elif initial_x > initial_y:
+                            diagram[(coord, (initial_x - initial_y))] += 1
+                            # print(f"diagram[(x: {coord}, y: {(initial_x - initial_y) + coord})] += 1")
+                elif initial_x > final_x:
+                    print(f"initial: x = {initial[0]}, y = {initial[1]} , final: x = {final[0]}, y = {final[1]}")
+                    print(f"Initial: {ins[0]}, final: {ins[1]}")
+                    for coord in range(initial_y, final_y +1):
+                        if initial_x < initial_y:    
+                            diagram[(initial_x, coord)] += 1
+                            print(f"diagram[(x: {coord}, y: {coord})] += 1")
+                            # print(f"diagram[(x: {initial_x}, y: {coord})] += 1")
 
-        # if initial_x == final_x or initial_y == final_y:
-        #     if initial_x < final_x:
-        #         for coord in range(initial_x, final_x):
-        #             m[f"x{coord}"] += 1
-        #     else:
-        #         for coord in range(final_x, initial_x):
-        #             m[f"x{coord}"] += 1
-        #     if initial_y < final_y:
-        #         for coord in range(initial_y, final_y):
-        #             m[f"y{coord}"] += 1
-        #     else:
-        #         for coord in range(final_y, initial_y):
-        #             m[f"y{coord}"] += 1
-        
-        
+                    
+
+
+
+            #     for coord in range(initial_y, final_y +1):
+            #         diagram[(initial_x, coord)] += 1
+            # elif initial_y > final_y:
+            #     for coord in range(initial_y, final_y -1, -1):
+            #         diagram[(initial_x, coord)] += 1
+
+            # elif initial_y == final_y:
+            #     if initial_x < final_x:
+            #         for coord in range(initial_x, final_x +1):
+            #             diagram[(coord, initial_y)] += 1
+
+            #     elif initial_x > final_x:
+            #         for coord in range(initial_x, final_x -1, -1):
+            #             diagram[(coord, initial_y)] += 1
+
     # result = 0
-    # for v in m.values():
+    # for v in diagram.values():
     #     if v >= 2:
     #         result += 1
-    
-    # print(result)
 
-        
+    # print(result)
 
 
 if __name__ == '__main__':
-    # data = get_input("./day_5/day_5_input.txt")
-    data = get_input("./day_5/day_5_test.txt")
+    data = get_input("./day_5/day_5_input.txt")
+    # data = get_input("./day_5/day_5_test.txt")
     part_1(draw_map(), data)
