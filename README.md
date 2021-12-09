ab593eada67ac3a9b0638694b495d217156594b9
0x5ubt13 <5ubt13@protonmail.com>
2021-12-09 17:26:00 +0000

    day 9 update

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # Advent of Code 2021 
 
 Please be welcomed to my solutions for Advent of Code 2021!! :smiley:
-This year I will try to complete every day at least with my strongest suit (Python) within the day and then attempt to complete them in Golang without a time constraint. 
+This year I will try to complete every day at least with my strongest suit (Python) within the day and save a record whether the target has been met. Then, attempt to complete them in Golang without a time constraint. 
 
-| Puzzle | Language |     Completed    |
-|:------:|:--------:|:----------------:|
-| [Day 1](./day_1)  |  [Python](./day_1/day_1.py)  |:heavy_check_mark:|
-|        |    [Go](./day_1/day_1.go)    |:heavy_check_mark:|
-| [Day 2](./day_2)  |  [Python](./day_2/day_2.py)  |:heavy_check_mark:|
-|        |    [Go](./day_2/day_2.go)    | :heavy_check_mark: |
-| [Day 3](./day_3)  |  [Python](./day_3/day_3.py)  |:heavy_check_mark:|
-|        |    [Go]()    | :woozy_face:  |
-| [Day 4](./day_4)  |  [Python](./day_4/day_4.py)  |:heavy_check_mark:|
-|        |    [Go]()    | :woozy_face:  |
-| [Day 5](./day_5)  |  [Python](./day_5/day_5.py)  |:woozy_face:|
-|        |    [Go]()    | :woozy_face:  |
-| [Day 6](./day_6)  |  [Python](./day_6/day_6.py)  |:heavy_check_mark:|
-|        |    [Go]()    | :woozy_face:  |
-| [Day 7](./day_7)  |  [Python](./day_7/day_7.py)  |:heavy_check_mark:|
-|        |    [Go]()    | :woozy_face:  |
+| Puzzle | Language |     Completed    | Target met on time |
+|:------:|:--------:|:----------------:|:----------:|
+| [Day 1](./day_1)  |  [Python](./day_1/day_1.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go](./day_1/day_1.go)    |:heavy_check_mark:|:heavy_check_mark:| 
+| [Day 2](./day_2)  |  [Python](./day_2/day_2.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go](./day_2/day_2.go)    |:heavy_check_mark:|:heavy_check_mark:|
+| [Day 3](./day_3)  |  [Python](./day_3/day_3.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go]()    | :woozy_face:  ||
+| [Day 4](./day_4)  |  [Python](./day_4/day_4.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go]()    | :woozy_face:  ||
+| [Day 5](./day_5)  |  [Python](./day_5/day_5.py)  |:woozy_face:|:star:|
+|        |    [Go]()    | :woozy_face:  ||
+| [Day 6](./day_6)  |  [Python](./day_6/day_6.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go]()    | :woozy_face:  ||
+| [Day 7](./day_7)  |  [Python](./day_7/day_7.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go]()    | :woozy_face:  ||
+| [Day 8](./day_7)  |  [Python](./day_8/day_8.py)  |:heavy_check_mark:|:star2::star2:|
+|        |    [Go]()    | :woozy_face:  ||
+| [Day 9](./day_7)  |  [Python](./day_9/day_9.py)  |:heavy_check_mark:|:star:|
+|        |    [Go]()    | :woozy_face:  ||
--- day_9/day_9.py
+++ day_9/day_9.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3D
 
 def get_input(filename):
     with open(filename, "r") as f:
@@ -7,36 +7,125 @@ def get_input(filename):
 def solve(matrix):
     low_points_risk = 0
     for row in range(len(matrix)):
+
         for column in range(len(matrix[row])):
-            try:
-                num, up, down, left, right = matrix[row][column], matrix[row-1][column], matrix[row+1][column], matrix[row][column-1], matrix[row][column+1]
-            except:
-                pass
+
             if column == 0: #First of the row
+                
                 if row == 0: # First row, first column
-                    if down < num and right < num:
+                    num, down, right = matrix[row][column], matrix[row+1][column], matrix[row][column+1]
+                    print(num, down, right)
+                    if down > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
                         low_points_risk += num + 1
-                elif row == len(matrix) -1:
-                    print(len(matrix), len(matrix) -1 ) 
+                elif row == len(matrix) -1: # Bottom row (last row), first number
+                    num, up, right = matrix[row][column], matrix[row-1][column], matrix[row][column+1]
+                    print(num, up, right)
+                    if up > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+                else:
+                    num, up, down, right = matrix[row][column], matrix[row-1][column], matrix[row+1][column], matrix[row][column+1]
+                    print(num, up, down, right)
+                    if up > num and down > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+                    
 
-            elif column == (len(matrix[row]) - 1): # Last of the row
+            elif column == (len(matrix[row])-1): # Last of the row
+                
                 if row == 0: # first row, Last column
-                    if left < num and down < num:
-                        low_points_risk += num + 1
-
-            else: # Any other middle number
-                if row == 0: # First row, no up numbers
-                    if left < num and down < num and right < num:
+                    num, down, left = matrix[row][column], matrix[row+1][column], matrix[row][column-1]
+                    print(num, down, left)
+                    if left > num and down > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
                         low_points_risk += num + 1
                 
+                elif row == len(matrix) -1: # Bottom row (last row), last number
+                    num, up, left = matrix[row][column], matrix[row-1][column], matrix[row][column-1]
+                    print(num, up, left)
+                    if up > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+
+                else: # Any middle
+                    num, up, down, left = matrix[row][column], matrix[row-1][column], matrix[row+1][column], matrix[row][column-1]
+                    print(num, up, down, left)
+                    if up > num and down > num and left > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+
+
+            else: # Any other middle number
+
+                if row == 0: # First row, no up numbers
+                    num, down, left, right = matrix[row][column], matrix[row+1][column], matrix[row][column-1], matrix[row][column+1]
+                    print(num, down, left, right)
+                    if left > num and down > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+
+                elif row == len(matrix) -1: # Bottom row
+                    num, up, left, right = matrix[row][column], matrix[row-1][column], matrix[row][column-1], matrix[row][column+1]
+                    print(num, up, left, right)
+                    if up > num and left > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+
+                else:
+                    num, up, down, left, right = matrix[row][column], matrix[row-1][column], matrix[row+1][column], matrix[row][column-1], matrix[row][column+1]
+                    print(num, up, down, left, right)
+                    if up > num and down > num and left > num and right > num:
+                        print(f"match at matrix[{row}][{column}] -> {num}", )
+                        low_points_risk += num + 1
+
+    # Part 2:
+    # boilerplate: num, up, down, left, right = matrix[row][column], matrix[row-1][column], matrix[row+1][column], matrix[row][column-1], matrix[row][column+1]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("part2:")
+            num = matrix[row][col]
+            down, right = matrix[row+1][col-1], matrix[row][col+1]
+            print(nu, down, right)
+            if matrix[row][col] != 9: # Main part 2 loop, 9s are discarded:
+                num = matrix[row][col]
+                basin, basins, ignore = 0, [], []
+                if column == 0: #First of the row
+                    if row == 0: # First row, first column, first number of the matrix [0][0]
+                        down, left, right = matrix[row-1][column], matrix[row+1][column], matrix[row][column-1], matrix[row][column+1]
+                        print(down)
+                        # Check adjacent numbers
+                        if down == num+1 or down == num-1:
+                            
+                            basins.append(set(row, col))
+                            basin = True
+                            while basin:
+                                basin+=1
+
+
+                    
+                #     elif row == len(matrix) -1: # Bottom row (last row), first number
+                    
+                #     else: # First num of any middle row
+
+                # elif column == (len(matrix[row])-1): # Last of the row
+                #     if row == 0: # first row, Last column
+
+                #     elif row == len(matrix) -1: # Bottom row (last row), last number
+
+                #     else: # Any middle
+
+                
+                # else: # Any other middle number
+                #     if row == 0: # First row, no numbers above
+                    
+                #     elif row == len(matrix) -1: # Bottom row, no numbers below
+
+                #     else: # The majority of middle numbers
 
 
 
-            #     print(f"first row: [{row}][{column}]", matrix[row][column])
-            # if row == 1:
-            #     print(f"second row: [row][column]", matrix[row][column])
-            # if i - 1 >= 0 and j - 1 >= 0:
-            #     print(data[i][j])
+    print("Part 1:", low_points_risk)
 
 
 if __name__ == '__main__':
