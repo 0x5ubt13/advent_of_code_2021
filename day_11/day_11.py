dfbbe28ba46f2aea50a6a8b4912ba61c12612c24
0x5ubt13 <5ubt13@protonmail.com>
2021-12-26 15:00:19 +0000

    day 11 solved py

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -1,91 +1,117 @@
 #!/usr/bin/python3
 
-with open('./day_11/11.test', 'r') as f:
-    grid = [[int(row.strip()) for row in column.strip()] for column in f]
+def get_input():
+    with open('./day_11/11.in', 'r') as f:
+        return [[int(row.strip()) for row in column.strip()] for column in f]
 
-glowing = False
-for step in range(1, 5):
-    print('Step', step)
 
-    for row in range(0, len(grid)):
+def plus_1(grid):
+    # First, the energy level of each octopus increases by 1.
+    for row in range(len(grid)):
         for col in range(len(grid)):
-            if grid[col][col] == 0:
-                glowing = True
+            grid[row][col] += 1   
 
-    while glowing == True:
-        for row in range(0, len(grid)):
-            for col in range(len(grid)):
-                check = 0
 
-                # Calculate adjacent to 0's
-                num = grid[row][col]
-                if num == 0:
-                    glowing = True
-                    try: 
-                        if grid[row-1][col] % 9 == 0: 
-                            grid[row-1][col] = 0 
-                            check += 1
-                        else: grid[row-1][col]+= 1
-                    except IndexError: pass                    
-                    try:
-                        if grid[row-1][col-1] % 9 == 0:
-                            grid[row-1][col-1] = 0 
-                            check += 1
-                        else: grid[row-1][col-1] += 1
-                    except IndexError: pass                    
-                    try: 
-                        if grid[row][col-1] % 9 == 0: 
-                            grid[row][col-1] = 0
-                            check += 1
-                        else: grid[row][col-1] += 1
-                    except IndexError: pass                    
-                    try: 
-                        if grid[row+1][col-1] % 9 == 0:
-                            grid[row+1][col-1] = 0
-                            check += 1
-                        else: grid[row+1][col-1] += 1
-                    except IndexError: pass                    
-                    try: 
-                        if grid[row+1][col] % 9 == 0:
-                            grid[row+1][col] = 0
-                            check += 1
-                        else: grid[row+1][col] += 1
-                    except IndexError: pass                    
-                    try: 
-                        if grid[row+1][col+1] % 9 == 0:
-                            grid[row+1][col+1] = 0
-                            check += 1
-                        else: grid[row+1][col+1] += 1
-                    except IndexError: pass                    
-                    try: 
-                        if grid[row][col+1] % 9 == 0: 
-                            grid[row][col+1] = 0
-                            check += 1
-                        else: grid[row][col+1] += 1
-                    except IndexError: pass
-                    try: 
-                        if grid[row-1][col+1] % 9 == 0:
-                            grid[row-1][col+1] = 0
-                            check += 1
-                        else: grid[row-1][col+1] += 1
-                    except IndexError: pass
-
-                    if check == 0:
-                        glowing = False
-
-    # Start next step: add 1 to every number and convert 9's in 0's
-    for row in range(0, len(grid)):
+def pepinazu(grid, flashed):
+    # Then, any octopus with an energy level greater than 9 flashes. 
+    # This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. 
+    # If this causes an octopus to have an energy level greater than 9, it also flashes. 
+    # This process continues as long as new octopuses keep having their energy level increased beyond 9. 
+    # (An octopus can only flash at most once per step.)
+    
+    for row in range(len(grid)):
         for col in range(len(grid)):
-            if grid[row][col] % 9 != 0 or grid[row][col] == 0:
-               grid[row][col] += 1
-            else:
+            if grid[row][col] > 9 and str(str(row)+str(col)) not in flashed:
+                flashed.append(str(str(row)+str(col)))
+                # Primera fila
+                if row == 0:
+                    if col == 0:
+                        grid[row+1][col] += 1
+                        grid[row][col+1] += 1
+                        grid[row+1][col+1] += 1
+                    elif col == 9:
+                        grid[row+1][col] += 1
+                        grid[row][col-1] += 1
+                        grid[row+1][col-1] += 1
+                    else:
+                        grid[row][col-1] += 1
+                        grid[row+1][col-1] += 1
+                        grid[row+1][col] += 1
+                        grid[row+1][col+1] += 1
+                        grid[row][col+1] += 1
+                # Ultima fila
+                elif row == 9:
+                    if col == 0:
+                        grid[row-1][col] += 1
+                        grid[row][col+1] += 1
+                        grid[row-1][col+1] += 1
+                    elif col == 9:
+                        grid[row][col-1] += 1
+                        grid[row-1][col] += 1
+                        grid[row-1][col-1] += 1
+                    else:
+                        grid[row][col-1] += 1
+                        grid[row-1][col-1] += 1
+                        grid[row-1][col] += 1
+                        grid[row-1][col+1] += 1
+                        grid[row][col+1] += 1
+                # Cualquiera del medio     
+                else:
+                    if col == 0:
+                        grid[row-1][col] += 1
+                        grid[row-1][col+1] += 1
+                        grid[row][col+1] += 1
+                        grid[row+1][col] += 1
+                        grid[row+1][col+1] += 1
+                    elif col == 9:
+                        grid[row-1][col] += 1 # arriba
+                        grid[row-1][col-1] += 1 # arriba izda
+                        grid[row][col-1] += 1 # izda
+                        grid[row+1][col] += 1 # abajo
+                        grid[row+1][col-1] += 1 # abajo izda
+                    else:
+                        grid[row-1][col] += 1 # arriba
+                        grid[row-1][col+1] += 1 # arriba dcha
+                        grid[row][col+1] += 1 # dcha
+                        grid[row+1][col+1] += 1 # abajo dcha
+                        grid[row+1][col] += 1 # abajo
+                        grid[row+1][col-1] += 1 # abajo izda
+                        grid[row][col-1] += 1 # izda
+                        grid[row-1][col-1] += 1 # arriba izda
+
+
+def resetea(grid, step):
+    part_2_counter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid)):
+            if grid[row][col] > 9:
+                part_2_counter += 1
+                if part_2_counter == 100:
+                    print(f"Part 2: {step}")
                 grid[row][col] = 0
-    
-    for line in grid: 
-        print(line)
-    
-    
-        
+
+
+if __name__ == '__main__':
+    counter = 0
+    grid = get_input() # pilla el input
+
+    for i in range(1, 1001): # pasos (objetivo parte 1: 100)
+        #print(f"Paso {i}:")
+        plus_1(grid) # suma 1
+
+        flashed = [] # lista de guarros 
+        for j in range(15):
+            pepinazu(grid, flashed) # si mas de 9, pepinazo
+
+        resetea(grid, i) # si mas de 9 = 0
+        for i in flashed:
+            counter += 1
+
+        # for line in grid:
+        #     print(line)
+        # else: print("")
+
+    print(counter)
 
 
 
