fccb2d70bc6a5f2d55c0598d1f0256f7a26688c5
0x5ubt13 <5ubt13@protonmail.com>
2022-03-15 14:52:55 +0000

    saving

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -2,13 +2,48 @@ package main
 
 import (
 	"fmt"
-
+	"os"
+	"bufio"
+	"io"
+	//"strings"
 )
 
 func main () {
-	getData
+	data := getLines("./day_3_input.txt")
+
+	fmt.Print(data)
+
+	for i, r := range data {
+		fmt.Println(i, r)
+		for j, c := range r {
+			fmt.Println(j, string(c))
+		}
+	}
 }
 
-func getData(filename string) []int64 {
-	
+func getLines(filename string) []string {
+	f, err := os.Open(filename)
+	chk(err)
+	defer f.Close()
+
+	data := make([]string, 0)
+
+	reader := bufio.NewReader(f)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		data = append(data, string(line))
+	}
+
+	return data
+}
+
+func chk(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
