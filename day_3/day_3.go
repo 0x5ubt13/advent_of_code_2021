b23a404fac949edbc629c684aacc3e3161d27846
0x5ubt13 <5ubt13@protonmail.com>
2022-03-26 18:54:55 +0000

    sync

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -5,20 +5,58 @@ import (
 	"os"
 	"bufio"
 	"io"
+	"strconv"
 	//"strings"
 )
 
 func main () {
 	data := getLines("./day_3_input.txt")
+	partOne(data)
+}
 
-	fmt.Print(data)
+func partOne(data []string) {
+	commonZeroes := make(map[int]int)
+	commonOnes := make(map[int]int)
+	mostCommon := make(map[int]int)
+	var mc string
+	lessCommon := make(map[int]int)
+	var lc string
 
 	for i, r := range data {
-		fmt.Println(i, r)
+		// whole binary numbers inside data.
+		fmt.Println(i, r) 
 		for j, c := range r {
-			fmt.Println(j, string(c))
+			// binary numbers index inside the numbers
+			// string(c) to avoid printing runes
+			//fmt.Println(j, string(c)) 
+			if string(c) == "0" {
+				commonZeroes[j] += 1
+			} else {
+				commonOnes[j] += 1
+			}
 		}
 	}
+	for k := 0; k < len(commonOnes); k++ {
+		if commonOnes[k] > commonZeroes[k] {
+			mostCommon[k] = 1
+			mc += "1"
+			lessCommon[k] = 0
+			lc += "0"
+		} else {
+			mostCommon[k] = 0
+			mc += "0"
+			lessCommon[k] = 1
+			lc += "1"
+		}
+	}
+	most, err := strconv.ParseInt(mc, 2, 10)
+	chk(err)
+
+	fmt.Println(most, lc)
+
+
+
+	
 }
 
 func getLines(filename string) []string {
