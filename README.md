31b2c8f04931293a97eaa9fd2b25b36b0ec8046a
0x5ubt13 <5ubt13@protonmail.com>
2022-03-31 17:40:21 +0100

    day 4 solved using go!! :)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Advent of Code 2021 
 
 Please be welcomed to my solutions for Advent of Code 2021!! :smiley:
-This year I will try to complete every day at least with my strongest suit (Python) within the day and save a record whether the target has been met. Then, attempt to complete them in Golang without a time constraint. 
+This year I will try to complete every day at least with my strongest suit (Python) within the day and save a record whether the target has been met. Then, attempt to complete them in Golang without a time constraint (ideally will mark with a check if solved before the next Advent of Code). 
 
 | Puzzle | Language |     Completed    | Target met on time |
 |:------:|:--------:|:----------------:|:----------:|
@@ -12,7 +12,7 @@ This year I will try to complete every day at least with my strongest suit (Pyth
 | [Day 3](./day_3)  |  [Python](./day_3/day_3.py)  |:heavy_check_mark:|:star2::star2:|
 |        |    [Go]()    | :heavy_check_mark:  ||
 | [Day 4](./day_4)  |  [Python](./day_4/day_4.py)  |:heavy_check_mark:|:star2::star2:|
-|        |    [Go]()    | :woozy_face:  ||
+|        |    [Go]()    | :heavy_check_mark:  |:heavy_check_mark:|
 | [Day 5](./day_5)  |  [Python](./day_5/day_5.py)  |:woozy_face:|:star:|
 |        |    [Go]()    | :woozy_face:  ||
 | [Day 6](./day_6)  |  [Python](./day_6/day_6.py)  |:heavy_check_mark:|:star2::star2:|
--- day_4/day_4.go
+++ day_4/day_4.go
@@ -8,50 +8,102 @@ import (
 )
 
 func main() {
-	rawBytes := getInput("day_4_test_input.txt")
+	rawBytes := getInput("day_4_input.txt")
 	lines := strings.Split(string(rawBytes), "\n")
-	solutionOne := partOne(lines)
-	fmt.Println(solutionOne)
+	partOne, partTwo := solve(lines)
+	fmt.Printf("Part 1 -> %d\n", partOne)
+	fmt.Printf("Part 2 -> %d\n", partTwo)
 }
 
-func partOne(lines []string) string {
+func solve(lines []string) (int, int) {
+	var result1, result2 int
 	numbersPool := strings.Split(lines[0], ",")
 	drawnNumbers := make([]int, 0)
+	winningBoardsTracker := make([]int, 0)
 	newRound(drawnNumbers, numbersPool)
 
 	// 0. create boards -> map int:[]int -> 1: board, 2: board, 3: board etc
 	playingBoards := playingBoards(lines)
-	// for i, b := range playingBoards {
-	// 	fmt.Println("Board number",i, ":")
-	// 	for _, board := range b {
-	// 		fmt.Println("\t\t", board)
-	// 	}
-	// }
-	
+
 	// Starting the game, main loop
 	for {
 		// 1. draw new number
 		drawnNumbers = newRound(drawnNumbers, numbersPool)
 		if drawnNumbers[len(drawnNumbers)-1] == 999 {
-			return "Exhausted numbers pool. Try harder."
+			break
 		}
 		fmt.Println("Numbers drawn:", drawnNumbers)
 		
 		// 2. loop drawn nums for every board possible. Check against shadow boolean board
-		for i, board := range playingBoards {
-			fmt.Printf("Board number #%d\n", i)
-			for row, column := range board {
+		for board, numbers := range playingBoards {
+			// Part 2 -> check that the board has already won, skip it if so
+			chk := numInSlice(board, winningBoardsTracker)
+			if chk == true {
+				continue
+			} 
+
+			// Create new shadow board for every new board
+			fmt.Printf("Board number #%d. Creating new shadow board.\n", board)
+			shadowBoard := newShadowBoard()
+
+			for row, column := range numbers {
 				for col, number := range column {
-					//test fmt.Printf("row %v, column %v, number %v\n", row, col, number)
-					
+					// fmt.Printf("row %v, column %v, number %v\n", row, col, number)
+					for _, drawnNum := range drawnNumbers {
+						if drawnNum == number {
+							shadowBoard[row][col] = true
+						}
+					}
 				}
+			}
+
+			for _, row := range shadowBoard {
+				fmt.Println(row)
+			} 
+			
+			// Getting some winning boards. Save the first for part 1 and continue until hitting the last one for part 2
+			part1Endgame := checkBoard(shadowBoard)
+
+			if part1Endgame == true {
+				winningBoardsTracker = append(winningBoardsTracker, board)
+				lastCall := drawnNumbers[len(drawnNumbers)-1]
+
+				if result1 == 0 {	
+					result1 = calculateResult(shadowBoard, playingBoards[board], lastCall)
+				}
+				
+				if len(winningBoardsTracker) == len(playingBoards) {
+					result2 = calculateResult(shadowBoard, playingBoards[board], lastCall)
+				}
+
+				part1Endgame = false
+			}
+
+			if result2 != 0 {
+				break
+			}
+		}
+
+		if result2 != 0 {
+			break
+		}
+		
+	} 
+
+	return result1, result2
+}
+
+func calculateResult(shadowBoard [5][5]bool, playingBoard [5][5]int, lastCall int ) int {
+	var result int
+	for row, cols := range shadowBoard {
+		for col, value := range cols {
+			if value == false {
+				result += playingBoard[row][col]
 			}
 		}
 	}
-}
 
-func partTwo() {
-
+	return result * lastCall
 }
 
 func newRound(drawnNumbers []int, numbersPool []string) []int {
@@ -62,7 +114,7 @@ func newRound(drawnNumbers []int, numbersPool []string) []int {
 
 func drawNewNumber(drawnNumbers []int, numbersLine []string) int {
 	for _, n := range numbersLine {
-		n, err := strconv.Atoi(n)
+		n, err := strconv.Atoi(strings.TrimSuffix(n, "\r"))
 		if err != nil{fmt.Println(err)}
 		if numInSlice(n, drawnNumbers) == false {
 			return n
@@ -76,7 +128,9 @@ func numInSlice(x int, y []int) bool {
 		if x == n {
 			return true
 		}
-	}; return false
+	}
+	
+	return false
 }
 
 func newBoard() [5][5]int {
@@ -101,15 +155,33 @@ func newShadowBoard() [5][5]bool {
 	return newShadowBoard
 }
 
-func checkBoard(drawnNumbers, board []int) bool {
-	var winner bool
-	
-	if winner == true {
-		fmt.Println("WE HAVE A WINNER!!!")
-		return true
-	} else {
-		return false
+func checkBoard(board [5][5]bool) bool {
+	winner := false
+	var row, col int
+
+	for i := 0; i < 5; i++ {
+		//reset loop
+		row = 0
+		col = 0
+
+		for j := 0; j < 5; j++ {
+			// horizontal check
+			if board[i][j] == true{ 
+				row++
+			}
+
+			// vertical check
+			if board[j][i] == true {
+				col++
+			}
+		}
+
+		if row == 5 || col == 5 {
+			winner = true
+		}
 	}
+
+	return winner
 }
 
 func playingBoards(lines []string) map[int][5][5]int {
