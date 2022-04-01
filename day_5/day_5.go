63f884bbdc3613eeb9969b5c3e097e32d1cd7458
0x5ubt13 <5ubt13@protonmail.com>
2022-04-01 20:36:00 +0100

    saving progress

--- day_5/day_5.go
+++ day_5/day_5.go
@@ -7,16 +7,33 @@ import(
 )
 
 func main() {
-	lines := getPuzzleInput("./day_5_test.txt")
-	for _, l := range lines {
-		fmt.Println(l)
-	}
+	getPuzzleInput("./day_5_test.txt")
+	// instructions := make(map[int][]int)
+
+	// for _, l := range lines {
+	// 	fmt.Println(l)
+	// }
 }
 
 func getPuzzleInput(filename string) []string {
 	bytes, err := ioutil.ReadFile(filename)
 	chkErr(err)
-	lines := strings.Split(string(bytes), "\n")
+	
+	lines := strings.Split(strings.ReplaceAll(string(bytes), " -> ", " "), "\n")
+	ins := make([]string, 0)
+
+	for _, line := range lines {
+		ins = append(ins, strings.Split(line, " ")...)
+	}
+
+	for i, l := range ins {
+		if i % 2 == 0 {
+			fmt.Printf("x1 = %v, y1 = %v  ", string(l[0]), string(l[2]))
+		} else {	
+			fmt.Printf("  x2 = %v, y2 = %v\n", string(l[0]), string(l[2]))
+		}
+	}
+
 	return lines
 }
 
