d7bfefff502c35e895821efe1ce47175ae2cf765
0x5ubt13 <5ubt13@protonmail.com>
2022-04-08 18:03:56 +0100

    day 8 part 1 golang complete

--- day_8/day_8.go
+++ day_8/day_8.go
@@ -7,7 +7,39 @@ import (
 )
 
 func main() {
-	lines := getPuzzleInput("./day_8_test.txt")
+	_, digits := getPuzzleInput("./day_8_input.txt")
+	
+	var ones, fours, sevens, eights int
+	hits := 0
+
+	for _, digit := range digits {
+		if len(digit) == 3 || len(digit) == 2 || len(digit) == 4 || len(digit) == 7 {
+			hits += 1
+		}
+
+		switch len(digit) {
+		case 2: // 1
+			ones++
+		case 3: // 7
+			sevens++
+		case 4: // 4
+			fours++
+		case 7: // 8
+			eights++
+		}
+	}
+
+	part1 := ones + fours + sevens + eights
+
+	fmt.Println("Part 1 ->", part1)
+}
+
+func getPuzzleInput(filename string) ([]string, []string) {
+	bytes, err := ioutil.ReadFile(filename)
+	chkErr(err)
+
+	lines := strings.Split(string(bytes), "\n")
+	
 	var sig, signals, dig, digits []string
 
 	for _, line := range lines {
@@ -15,37 +47,15 @@ func main() {
 		dig = append(dig, strings.Split(line, " | ")[1])
 	}
 
-
-
 	for _, signal := range sig {
-		signals = append(signals, strings.Split(signal, " ")...)
+		signals = append(signals, strings.Split(strings.TrimSuffix(signal, "\r"), " ")...)
 	} 
 
 	for _, digit := range dig {
-		digits = append(digits, strings.Split(digit, " ")...)
+		digits = append(digits, strings.Split(strings.TrimSuffix(digit, "\r"), " ")...)
 	} 
 
-	fmt.Println(signals[2], digits[2])
-	// for i, line := range signals {
-	// 	for j, signal := range line {
-	// 		fmt.Printf("Line %d, signal %d -> %s\n", i, j, string(signal))
-	// 	}
-	// }
-
-
-
-
-}
-
-func getPuzzleInput(filename string) []string {
-	bytes, err := ioutil.ReadFile(filename)
-	chkErr(err)
-
-	lines := strings.Split(string(bytes), "\n")
-
-	// fmt.Println(lines)
-	
-	return lines
+	return signals, digits
 }
 
 func chkErr(err error) {
