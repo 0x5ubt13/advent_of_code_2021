a57dd0c561ed037cf673ba9d69f35218f7c6ce85
0x5ubt13 <5ubt13@protonmail.com>
2022-04-12 12:48:58 +0100

    day 8 finished in go!!!! :)

--- day_8/day_8.go
+++ day_8/day_8.go
@@ -3,14 +3,250 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
 	"strconv"
+	"strings"
+	// "strconv"
 )
 
 func main() {
-	signals, digits := getPuzzleInput("./day_8_input.txt")
+	partOne()
+	partTwo()
+}
+
+func partTwo() {
+	// signals[0] -> same line than digits[0]
+	_, _, signals, digits := getPuzzleInput("./day_8_input.txt")
+
+	decoded := make([]string, 0, 0)
+
+	for line := 0; line < len(signals); line++ {
+		// line by line
+		var match int
+		decoding := make(map[int]string)
+
+
+		fmt.Printf("Line %d --> %v | %v\n", line, signals[line], digits[line])
+
+		currentLine := strings.Split(strings.TrimSuffix(signals[line], "\r"), " ")
+
+		for _, signal := range currentLine {
+			// Make sure we populate 1, 4, 7 and 8 first
+			switch len(signal) {
+			case 2: //1
+				decoding[1] = signal
+			case 3: // 7
+				decoding[7] = signal
+			case 4: // 4
+				decoding[4] = signal
+			case 7: // 8
+				decoding[8] = signal
+			}
+		}
+
+		for _, signal := range currentLine {
+			// Populate 0, 2, 3, 5, 6 and 9
+			switch len(signal) {
+			case 6: // 9, 0, 6
+				// 9
+				match = 0 
+				for _, r := range signal {
+					if rInStr(r, decoding[4]) == true {
+						match++
+					}
+				}
+
+				if match == 4 {
+					decoding[9] = signal
+					break
+				}
+
+				// 0
+				match = 0
+				for _, r := range signal {
+					if rInStr(r, decoding[1]) == true {
+						match++
+					} 
+				}
+
+				if match == 2 {
+					decoding[0] = signal
+					break
+				}
+
+				// 6
+				decoding[6] = signal
+			
+			case 5:
+				// 3
+				match = 0
+				for _, r := range signal {
+					if rInStr(r, decoding[1]) == true {
+						match++
+					}
+				}
+
+				if match == 2 {
+					decoding[3] = signal
+					break
+				}
+				
+				// 5
+				var match4, match7 int
+				for _, r := range signal {
+					if rInStr(r, decoding[7]) == true {
+						match7++
+					}
+
+					if rInStr(r, decoding[4]) == true {
+						match4++
+					}
+				}
+
+				if match4 == 3 && match7 == 2 {
+					decoding[5] = signal
+					break
+				}
+
+				// 2
+				decoding[2] = signal
+			}
+		}
+
+		// decoding populated, now we need to decode the digits
+		currentDigitsLine := strings.Split(strings.TrimSuffix(digits[line], "\r"), " ")
+		digitsLine := ""
+		for _, digit := range currentDigitsLine {
+			// Make sure we populate 1, 4, 7 and 8 first
+			switch len(digit) {
+			// Easy ones
+			case 2: //1
+				digitsLine += "1"
+			case 3: // 7
+				digitsLine += "7"
+			case 4: // 4
+				digitsLine += "4"
+			case 7: // 8
+				digitsLine += "8"
+
+			// Complicated ones
+			case 5:
+				var match3, match5, match2 int
+				for _, r := range digit {
+					for _, ru := range decoding[3] {
+						if r == ru {
+							match3++
+						}
+					}
+
+					for _, ru := range decoding[5] {
+						if r == ru {
+							match5++
+						}
+					}
+
+					for _, ru := range decoding[2] {
+						if r == ru {
+							match2++
+						}
+					}
+				}
+
+				if match2 == 5 {
+					digitsLine += "2"
+				} else if match3 == 5 {
+					digitsLine += "3"
+				} else if match5 == 5 {
+					digitsLine += "5"
+				} else {
+					panic("You're doing something wrong")
+				}
+			case 6:
+				var match0, match6, match9 int
+				for _, r := range digit {
+					for _, ru := range decoding[0] {
+						if r == ru {
+							match0++
+						}
+					}
+
+					for _, ru := range decoding[6] {
+						if r == ru {
+							match6++
+						}
+					}
+
+					for _, ru := range decoding[9] {
+						if r == ru {
+							match9++
+						}
+					}
+				}
+
+				if match0 == 6 {
+					digitsLine += "0"
+				} else if match6 == 6 {
+					digitsLine += "6"
+				} else if match9 == 6 {
+					digitsLine += "9"
+				} else {
+					panic("You're doing something wrong")
+				}
+			}
+		}
+
+		decoded = append(decoded, digitsLine)
+
+		for i := 0; i < len(currentLine); i++ {
+			fmt.Printf("%s -> %d\n", decoding[i], i)
+		}
+
+		// End of line
+	}
+
+	// End of input
 	
-	var ones, fours, sevens, eights, match, hits, checks, part1, part2 int
+	fmt.Println(decoded)
+
+	var solution int
+	
+	for _, number := range decoded {
+		sol, err := strconv.Atoi(number)
+		chkErr(err)
+
+		solution += sol
+	}
+
+	fmt.Println(solution)
+}
+
+func findSignals(digit, signal string) int {
+	matches := 0
+	
+	for _, r := range digit {
+		for _, ru := range signal {
+			if r == ru {
+				matches++
+			}
+		}
+	}
+
+	return matches
+}
+
+
+func rInStr(x rune, y string) bool {
+	for _, s := range y {
+		if s == x {
+			return true
+		}
+	}
+	return false
+}
+
+func partOne() {
+	_, digits, _, _ := getPuzzleInput("./day_8_input.txt")
+	
+	var ones, fours, sevens, eights, hits, part1 int
 
 	for _, digit := range digits {
 		if len(digit) == 3 || len(digit) == 2 || len(digit) == 4 || len(digit) == 7 {
@@ -32,133 +268,9 @@ func main() {
 	part1 = ones + fours + sevens + eights
 
 	fmt.Println("Part 1 ->", part1)
-
-	// Part 2
-	decoding := make(map[string]int, len(signals))
-	// decoded := make(map[int]int, len(signals))
-	decoded := make([]string, len(signals))
-
-	// signals[0] pairs with digits[0]
-	for i:= 0; i < len(signals); i++ {
-
-		for _, signal := range signals {
-			switch len(signal) {
-			case 2: // 1
-				decoding[signal] = 1
-			case 3: // 7
-				decoding[signal] = 7
-			case 4: // 4
-				decoding[signal] = 4
-			case 5: // 2, 3, 5
-				for k, v := range decoding { 
-					if v == 1 { // 3
-						checks += 1
-						match = 0
-						for _, letter := range k {
-							for _, lett := range signal {
-								if letter == lett {
-									match += 1
-								}
-							}
-						}
-
-						if match == 2 {
-							decoding[signal] = 3
-							break
-						}
-
-					} else if v == 4 || v == 7 {
-						match = 0
-						checks += 1
-						for _, letter := range k {
-							for _, lett := range signal {
-								if letter == lett {
-									match += 1
-								}
-							}
-						}
-
-						if v == 4 && match == 3 || v == 7 && match == 2 {
-							decoding[signal] = 4
-							break
-						}
-
-					} else {
-						if checks >= 2 {
-							decoding[signal] = 2
-							checks = 0
-							break
-						}
-					}
-				}
-
-			case 6: // 0, 6, 9 
-				for k, v := range decoding {
-					if v == 7 || v == 4 { // 9
-						match = 0
-						checks += 1
-						for _, letter := range k {
-							for _, lett := range signal {
-								if letter == lett {
-									match += 1
-								}
-							}
-						}
-
-						if v == 7 && match == 3 || v == 4 && match == 4 {
-							decoding[signal] = 9
-							break
-						} 
-
-					} else if v == 1 {
-						match = 0
-						checks += 1
-						for _, letter := range k {
-							for _, lett := range signal {
-								if letter == lett {
-									match += 1
-								}
-							}
-						}
-
-						if match == 2 {
-							decoding[signal] = 0
-							break
-						}
-					} else {
-						if checks >= 2 {
-							decoding[signal] = 6
-							checks = 0
-							break
-						}
-					}
-				}
-
-			case 7: // 8
-				decoding[signal] = 8
-			}
-		}
-
-		for _, v := range decoding {
-			decoded = append(decoded, strconv.Itoa(v))
-		}
-
-		decodedNumber, err := strconv.Atoi(strings.Join(decoded, " "))
-		chkErr(err)
-
-		part2 += decodedNumber
-
-
-	}
-
-	fmt.Println("Part 2 ->", part2)
-	
-
-
-
 }
 
-func getPuzzleInput(filename string) ([]string, []string) {
+func getPuzzleInput(filename string) ([]string, []string, []string, []string) {
 	bytes, err := ioutil.ReadFile(filename)
 	chkErr(err)
 
@@ -179,7 +291,7 @@ func getPuzzleInput(filename string) ([]string, []string) {
 		digits = append(digits, strings.Split(strings.TrimSuffix(digit, "\r"), " ")...)
 	} 
 
-	return signals, digits
+	return signals, digits, sig, dig
 }
 
 func chkErr(err error) {
