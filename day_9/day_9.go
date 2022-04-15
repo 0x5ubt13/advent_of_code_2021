bc60d163d39b6b1a648c4d80bf9b18821aadae5b
0x5ubt13 <5ubt13@protonmail.com>
2022-04-15 13:21:02 +0100

    finished conditions skeleton

--- day_9/day_9.go
+++ day_9/day_9.go
@@ -8,30 +8,67 @@ import (
 )
 
 func main() {
-	lines := getPuzzleInput("9.test")
+	heightMap, shadowMap := getPuzzleInput("9.test")
 
-	for _, line := range lines {
-		fmt.Println(line)
-	}
-
-
-	// heightmap := makeHeightmap(lines)
-
-	// for k, v := range heightmap {
-	// 	fmt.Println(k, v)
-	// }
-}
-
-func makeHeightmap(lines []int) map[int][]int {
-	heightmap := make(map[int][]int, len(lines))
-	// for i, row := range lines {}
-		
-
-	return heightmap
+	partOne(heightMap, shadowMap)
 
 }
 
-func getPuzzleInput(filename string) []int {
+func partOne(heightMap map[int][]int, shadowMap map[int][]bool) {
+	for row := 0; row < len(heightMap); row++ {
+		// Outer loop 
+		for column := 0; column < len(heightMap[0]); column++ {
+			// Inner loop. Column hit. Print where are we
+			fmt.Println("Row", row, "Column", column, "->", heightMap[row][column], shadowMap[row][column])
+
+			// Start row conditions:
+			if row == 0 {
+				// Top row. Condition -> no up
+
+				// Start column conditions:
+				if column == 0 {
+					// First column. Condition -> no left
+				} else if column == len(heightMap[0]) - 1 {
+					// Last column. Condition -> no right
+				} else {
+					// Any other column in the middle
+				}
+
+			} else if row == len(heightMap) - 1 { 
+				// Bottom row. Condition -> no down
+
+				// Start column conditions:
+				if column == 0 {
+					// First column. Condition -> no left
+				} else if column == len(heightMap[0]) - 1 {
+					// Last column. Condition -> no right
+				} else {
+					// Any other column in the middle
+				}
+
+			} else {
+				// Any other row in the middle
+
+				// Start column conditions:
+				if column == 0 {
+					// First column. Condition -> no left
+				} else if column == len(heightMap[0]) - 1 {
+					// Last column. Condition -> no right
+				} else {
+					// Any other column in the middle
+				}
+			}
+		}
+
+		// end of row
+	} 
+
+	// end of matrix
+
+}
+
+
+func getPuzzleInput(filename string) (map[int][]int, map[int][]bool) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
@@ -39,18 +76,22 @@ func getPuzzleInput(filename string) []int {
 
 	lines := strings.Split(string(bytes), "\n")
 
-	intLines := make([]int, 0, 0)
-	for _, line := range lines {
+	boolLines := make(map[int][]bool, len(lines))
+	intLines := make(map[int][]int, len(lines))
+	for i, line := range lines {
+
 		for _, num := range line {
 			casting, err := strconv.Atoi(strings.TrimSuffix(string(num), "\r"))
 			if err != nil {
-				fmt.Println(err)
+				// fmt.Println(err)
+			} else {
+				intLines[i] = append(intLines[i], casting)
+				boolLines[i] = append(boolLines[i], false)
 			}
-			intLines = append(intLines, casting)
 		}
 
 	}
 
-	return intLines
+	return intLines, boolLines
 
 }
