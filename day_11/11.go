3d2314db86c1dd00dce08478772400e58de1e960
0x5ubt13 <5ubt13@protonmail.com>
2022-04-23 13:46:30 +0100

    day 11 finished go

--- day_11/11.go
+++ day_11/11.go
@@ -8,97 +8,132 @@ import (
 )
 
 func main() {
-	grid, _ := getPuzzleInput("./11.test")
-	part1 := 3
+	grid, flashed := getPuzzleInput("./11.in")
+	part1 := 250
+	flashes := 0
 
-	maxY := len(grid)
-	maxX := len(grid[0])
-		
+	maxY := len(grid)-1
+	maxX := len(grid[0])-1
+	
+	loop:
 	for steps := 0; steps < part1; steps++ {
-		fmt.Println("Step", steps)
-		for i := 0; i < len(grid); i++ {
-			fmt.Println(grid[i])
-		}
+		// Uncomment following snippet to graphically view the grid:
+		// fmt.Println("Step", steps)
+		// for i := 0; i < len(grid); i++ {
+		// 	fmt.Println(grid[i])
+		// }
 
 		// 1: new step, add 1 to every octopus
+		part2 := 0
 		for y := 0; y < len(grid); y++ {
 			for x := 0; x < len(grid[0]); x++ {
+				if grid[y][x] == 0 {
+					part2++
+				}
+
+				if part2 == 100 {
+					fmt.Println("Part 2 ->", steps)
+					break loop
+				}
 				// fmt.Printf("Row %d, Column %d, Value of the octopus -> %d\n", y, x, data[y][x])
 				grid[y][x]++
 			}
 		}
 
-		// 2: if 9 or higher, make all neighbours +1
-		for y := 0; y < len(grid); y++ {
-			for x := 0; x < len(grid[0]); x++ {
-				positions := []string{"north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest"}
-				// positions := []string{""}
-				if grid[y][x] > 9 {
-					// top-left corner: Only east, southeast, south
-					if y == 0 {
-						positions = append(positions, "south")
-					}
+		// 2: if higher than 9, make all neighbours +1, only once for step
+		for z := 0; z < 20; z++ {
+			// Make sure every single one of them has the chance to flash
+			for y := 0; y < len(grid); y++ {
+				for x := 0; x < len(grid[0]); x++ {
+					
+					positions := []string{"north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest"}
+					removing := make([]string, 0)
 
-					if x == maxX {
+					if grid[y][x] > 9 && flashed[y][x] == false {
+						// Add to flashed:
+						flashed[y][x] = true
+						flashes++
 
-					}
+						// 4-way check:
+						// Top row: eliminate north
+						if y == 0 {
+							removing = append(removing, "north", "northeast", "northwest")
+						}
 
+						// Bottom row: eliminate south
+						if y == maxY {
+							removing = append(removing, "south", "southeast", "southwest")
+						}
 
+						// First column: eliminate west
+						if x == 0 {
+							removing = append(removing, "northwest", "west", "southwest")
+						}
 
-					for _, pos := range positions {
-						switch pos {
-						case "north":
-							grid[y+1][x]++
-						case "northeast":
-							grid[y+1][x+1]++
-						case "east":
-							grid[y][x+1]++
-						case "southeast":
-							grid[y+1][x+1]++
-						case "south":
-							grid[y+1][x]++
-						case "southwest":
-							grid[y+1][x-1]++
-						case "west":
-							grid[y][x-1]++
-						case "northwest":
-							grid[y-1][x-1]++
+						// Last column: eliminate east
+						if x == maxX {
+							removing = append(removing, "northeast", "east", "southeast")
+						}
+
+						positions = removeStrFromSlice(positions, removing)
+						// fmt.Printf("Row %d, Column %d, Value of the octopus -> %d\n", y, x, grid[y][x])
+						// fmt.Printf("\nPositions: %+v, Removing: %+v\n", positions, removing)
+						for _, pos := range positions {
+							switch pos {
+							case "north":
+								grid[y-1][x]++
+							case "northeast":
+								grid[y-1][x+1]++
+							case "east":
+								grid[y][x+1]++
+							case "southeast":
+								grid[y+1][x+1]++
+							case "south":
+								grid[y+1][x]++
+							case "southwest":
+								grid[y+1][x-1]++
+							case "west":
+								grid[y][x-1]++
+							case "northwest":
+								grid[y-1][x-1]++
+							}
 						}
 					}
-					
-					
-
 				}
 			}
 		}
 
-		// 2: any octopus with an energy level greater than 9 flashes. 
-		// This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. 
-		// If this causes an octopus to have an energy level greater than 9, it also flashes. 
-		// This process continues as long as new octopuses keep having their energy level increased beyond 9. 
+		// 3: if > 9, restore to 0 
 		// (An octopus can only flash at most once per step.)
 		
 		for y := 0; y < len(grid); y++ {
 			for x := 0; x < len(grid[0]); x++ {
 				// fmt.Printf("Row %d, Column %d, Value of the octopus -> %d\n", y, x, grid[y][x])
+				if flashed[y][x] == true {
+					flashed[y][x] = false
+					grid[y][x] = 0
+				}
 			}
 		}
-	}
 		
-	
-	
+		if steps == 99 {
+			fmt.Println("Part 1 ->", flashes)
+		}
+	}
 }
+
 
 func removeStrFromSlice(s []string, r []string) []string {
 	for _, w := range r {
 		for k, v := range s {
 			if w == v {
-				return append(s[:k], s[k+1:])
+				s = append(s[:k], s[k+1:]...)
 			}
 		}
 	}
+	
+	return s
 }
-
 
 func getPuzzleInput(filename string) (map[int][]int, map[int][]bool){
 	bytes, _ := ioutil.ReadFile(filename)
