a2a72963445341c784cd909535856b1bd5b93801
0x5ubt13 <5ubt13@protonmail.com>
2022-05-02 15:14:14 +0100

    part 1 complete

--- day_14/14.go
+++ day_14/14.go
@@ -7,23 +7,96 @@ import (
 )
 
 func main() {
-	template, rules := getPuzzleInput("./14.test")
+	polymer, rules := getPuzzleInput("./14.test")
+	fmt.Printf("Template:\t%s\n", polymer)
 
-	fmt.Println(template)
-	for _, l := range rules {
-		fmt.Printf("%v\n", l)
+	instructions := make(map[int][]string, 0)
+	for i, rule := range rules {
+		instructions[i] = strings.Split(rule, " -> ")
 	}
 
 	// Pair insertion. [N (N] [C) B] -> [N C (N] B [C) H B]
-	result := insertion(template, rules)
-	fmt.Println(result)
+	for steps := 0; steps <= 40; steps++ {
+		polymer = insertion(polymer, instructions)
+		fmt.Printf("After step %d:\t%d\n", steps, len(polymer))
+		if steps == 9 {
+			result := countElements(polymer)
+			fmt.Printf("Part 1 -> %d\n", result)
+		}
+
+		if steps == 40 {
+			result := countElements(polymer)
+			fmt.Printf("Part 2 -> %d\n", result)
+		}
+	}
+	
 }
 
-func insertion(template string, rules []string) string {
-	// Check the template and grab elements 2 by 2
-	for _, ch := range template {
-
+func countElements(polymer string) int {
+	occurrences := make(map[string]int)
+	
+	for _, ch := range polymer {
+		occurrences[string(ch)]++
 	}
+
+	fmt.Println(occurrences)
+
+	var mostCommon, leastCommon int
+
+	for _, v := range occurrences {
+		if leastCommon == 0 {
+			leastCommon = v
+		}
+
+		if v > mostCommon {
+			mostCommon = v
+		} else if v < leastCommon {
+			leastCommon = v
+		}
+	}
+
+	return mostCommon - leastCommon
+}
+
+func insertion(template string, instructions map[int][]string) string {
+	separated := make([]string, 0)
+	finalString := make([]string, 0)
+	
+	var newString string
+	
+	for i, ch := range template {
+		if i+1 < len(template) {
+			newString = string(ch) + string(template[i+1])
+		}
+		
+		separated = append(separated, newString)
+		newString = ""
+	}
+
+	// for i, rule := range rules {
+	// 	instructions[i] = strings.Split(rule, " -> ")
+	// }
+
+	// Pair insertion. [N (N] [C) B] -> [N C (N] B [C) H B]
+	for i, str := range separated {
+
+		for _, instruction := range instructions {
+
+			if str == instruction[0] {
+				myStr := string(str[0]) + instruction[1]
+
+				if i == len(separated)-2 {
+					myStr += string(str[1])
+				}
+
+				finalString = append(finalString, myStr)
+			}
+		}
+
+		
+	}
+
+	return strings.Join(finalString, "") 
 }
 
 func getPuzzleInput(filename string) (string, []string) {
