b6ae5946fe385b39cffc654fd1a81056e2033572
0x5ubt13 <5ubt13@protonmail.com>
2022-05-08 13:04:35 +0100

    sync

--- day_15/15.go
+++ day_15/15.go
@@ -21,7 +21,6 @@ func main() {
 	// 1: Mark all nodes unvisited.
 	// 1.2. Create a set of all the unvisited nodes called the unvisited set.
 	nodeDistance := make(map[Coord]int)
-	queue := make([]Coord, 0)
 
 	// 2. Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all other nodes.
 	//    The tentative distance of a node v is the length of the shortest path discovered so far between the node v and the starting node.
@@ -32,7 +31,6 @@ func main() {
 
 	for y := 0; y < maxY; y++ {
 		for x := 0; x < maxX; x++ {
-			queue = append(queue, Coord{y, x})
 			// "Zero for out initial node"
 			if y == 0 && x == 0 {
 				nodeDistance[Coord{y, x}] = 0
@@ -43,15 +41,13 @@ func main() {
 	}
 	visited := make(map[Coord]bool)
 
-	for len(queue) > 1 {
-		// pop current off the queue and mark it visited
-		current := queue[0]
-		queue = queue[1:]
+	current := Coord{0, 0}
+
+	for {
+		// mark current visited
 		fmt.Println(current)
 
-		if visited[current] == false {
-			visited[current] = true
-		} else {
+		if visited[current] == true {
 			continue
 		}
 
@@ -78,14 +74,24 @@ func main() {
 
 		}
 
+	
+
 		// 4. When we are done considering all of the unvisited neighbors of the current node, mark the current node as visited and remove it from the unvisited set.
 		//    A visited node will never be checked again (this is valid and optimal in connection with the
 		//    behavior in step 6.: that the next nodes to visit will always be in the order of 'smallest distance from initial node first'
 		//    so any visits after would have a greater distance).
 
+		visited[current] = true
+		
+
+		
+
 		// 5. If the destination node has been marked visited (when planning a route between two specific nodes) or if the smallest tentative distance among the nodes in the
 		//    unvisited set is infinity (when planning a complete traversal; occurs when there is no connection between the initial node and remaining unvisited nodes), then stop.
 		//    The algorithm has finished.
+		
+
+
 
 		// 6. Otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new current node, and go back to step 3.
 
