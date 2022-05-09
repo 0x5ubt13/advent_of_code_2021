57f3c40286f4248201beffea140db788bd8d38bd
0x5ubt13 <5ubt13@protonmail.com>
2022-05-09 14:22:32 +0100

    day 15 finished

--- day_15/15.go
+++ day_15/15.go
@@ -3,20 +3,112 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Coord struct {
 	Y, X int
 }
 
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
 func main() {
-	grid := getGrid("./15_test.txt")
+	grid := getGrid("./15_in.txt")
 	maxY := len(grid)
 	maxX := len(grid[0])
 
+	part1 := time.Now()
+	partOne(grid, maxY, maxX)
+	elapsed := time.Since(part1)
+	log.Printf("%s took %s", "Part1", elapsed)
+
+
+	
+	maxY2 := maxY * 5
+	maxX2 := maxX * 5
+	defer timeTrack(time.Now(), "part2")
+	partTwo(grid, maxY2, maxX2)
+
+}
+
+func partTwo(grid map[int][]int, maxY, maxX int) {
+	nodeDistance := make(map[Coord]int)
+	visited := make(map[Coord]bool)
+
+	tentativeDistance := 999999999999
+	
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			// "Zero for out initial node"
+			if y == 0 && x == 0 {
+				nodeDistance[Coord{y, x}] = 0
+			} else {
+				nodeDistance[Coord{y, x}] = tentativeDistance
+			}
+		}
+	}
+
+	rows := len(grid)
+	cols := len(grid[0])
+	
+	current := Coord{0,0}
+
+	for {
+		if visited[current] == true {
+			continue
+		}
+
+		neighbours := checkNeighbours(current, maxY, maxX)
+		for _, neighbour := range neighbours {
+			if visited[neighbour] == true {
+				continue
+			}
+
+			// Part 2 special: increment the grid 5 times in both dimensions using modulus
+
+			y := neighbour.Y % rows // capping to length of rows
+			x := neighbour.X % cols // capping to length of cols
+			val := grid[y][x] + neighbour.Y / rows + neighbour.X / cols // update values accordingly
+			if val > 9 { 
+				val -= 9 
+			} 
+
+			newDistance := nodeDistance[current] + val
+			if newDistance < nodeDistance[neighbour] {
+				nodeDistance[neighbour] = newDistance
+			}
+		}
+
+		visited[current] = true
+		
+		if visited[Coord{maxY-1, maxX-1}] == true {
+			fmt.Println(nodeDistance[Coord{maxY-1, maxX-1}])
+			break
+		} else {
+			minCoord := Coord{maxY, maxX}
+			minDistance := math.MaxInt
+
+			for k, v := range nodeDistance {
+				// fmt.Println(k, v)
+				if v < minDistance && !visited[k] {
+					minDistance = v
+					minCoord = k
+				}
+			}
+
+			current = minCoord
+		}
+	}
+}
+
+func partOne(grid map[int][]int, maxY, maxX int) {
 	// Dijksra's algorithm:
 	// 1: Mark all nodes unvisited.
 	// 1.2. Create a set of all the unvisited nodes called the unvisited set.
@@ -44,7 +136,7 @@ func main() {
 	current := Coord{0, 0}
 
 	for {
-		fmt.Println(current)
+		// fmt.Println(current)
 
 		if visited[current] == true {
 			continue
@@ -87,21 +179,17 @@ func main() {
 			minDistance := math.MaxInt
 
 			for k, v := range nodeDistance {
-				fmt.Println(k, v)
-				if v < minDistance {
+				// fmt.Println(k, v)
+				if v < minDistance && !visited[k] {
 					minDistance = v
 					minCoord = k
 				}
 			}
 
 			current = minCoord
-			fmt.Println("new current", current)
+			// fmt.Println("new current", current)
 		}
-
-
-
 	}
-
 }
 
 // Check the neighbours of the current coordinate with a 4-way check
@@ -126,104 +214,6 @@ func checkNeighbours(current Coord, maxY, maxX int) []Coord {
 
 	return neighbours
 }
-
-// // Starting over
-// func oldmain() {
-// 	maxY := len(grid)
-// 	maxX := len(grid[0])
-// 	var previous string
-// 	var risk int
-
-// 	queue := []Coord{{Y: 0, X: 0}}
-
-// 	// for len(queue) > 0 {
-// 	for i := 0; i < 10; i++ {
-// 		cur := queue[0]
-// 		queue = queue[1:]
-
-// 		// Stop the queue if we reached maxY and maxX
-// 		if cur.Y == maxY-1 && cur.X == maxX-1 {
-// 			break
-// 		}
-
-// 		// 4-way check:
-// 		// First check if they exist before comparing
-// 		existing := make(map[string]int, 0)
-
-// 		// Top exists
-// 		if cur.Y > 0 && previous != "top" {
-// 			existing["top"] = grid[cur.Y - 1][cur.X]
-// 		}
-
-// 		// Bottom exists
-// 		if cur.Y < maxY-1 && previous != "bottom" {
-// 			existing["bottom"] = grid[cur.Y + 1][cur.X]
-// 		}
-
-// 		// Left exists
-// 		if cur.X > 0 && previous != "left" {
-// 			existing["left"] = grid[cur.Y][cur.X - 1]
-// 		}
-
-// 		// Right exists
-// 		if cur.X < maxX-1 && previous != "right" {
-// 			existing["right"] = grid[cur.Y][cur.X + 1]
-// 		}
-
-// 		// Compare the ones that exist
-// 		minValue := 10
-// 		safestPosition := ""
-
-// 		for positionKey, riskValue := range existing {
-// 			if riskValue < minValue {
-// 				safestPosition = positionKey
-// 			}
-// 		}
-
-// 		// Transform safest position to coordinates
-// 		// and continue the path via the safest position
-// 		nextCoord := Coord{}
-// 		if safestPosition == "top" {
-
-// 			nextCoord = Coord{Y: cur.Y-1, X: cur.X}
-// 			previous = "bottom"
-// 			risk += grid[nextCoord.Y][nextCoord.X]
-
-// 			queue = append(queue, Coord{Y: cur.Y-1, X: cur.X})
-// 			fmt.Println("new position:", nextCoord)
-
-// 		} else if safestPosition == "bottom" {
-
-// 			nextCoord = Coord{Y: cur.Y+1, X: cur.X}
-// 			previous = "top"
-// 			risk += grid[nextCoord.Y][nextCoord.X]
-
-// 			queue = append(queue, nextCoord)
-// 			fmt.Println("new position:", nextCoord)
-
-// 		} else if safestPosition == "left" {
-
-// 			nextCoord = Coord{Y: cur.Y, X: cur.X-1}
-// 			previous = "right"
-// 			risk += grid[nextCoord.Y][nextCoord.X]
-
-// 			queue = append(queue, nextCoord)
-// 			fmt.Println("new position:", nextCoord)
-
-// 		} else if safestPosition == "right" {
-
-// 			nextCoord = Coord{Y: cur.Y, X: cur.X+1}
-// 			previous = "left"
-// 			risk += grid[nextCoord.Y][nextCoord.X]
-// 			queue = append(queue, nextCoord)
-
-// 			fmt.Println("new position:", nextCoord)
-// 		}
-
-// 	}
-
-// 	fmt.Println(risk)
-// }
 
 func getGrid(filename string) map[int][]int {
 	bytes, err := ioutil.ReadFile(filename)
