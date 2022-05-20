cfd96cfb64e190c69878bad17bcf2ae589bdf06e
0x5ubt13 <5ubt13@protonmail.com>
2022-05-20 12:19:38 +0100

    part 1 solved

--- day_16/bitwise16.go
+++ day_16/bitwise16.go
@@ -1,22 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
 
 var Count int
 
 func main() {
+	// bits := []byte{
+	// 	// 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0,
+	// 	// 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	// 	// 1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,
+	// }
 
-	bits := []byte{
-		// 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0,
-		0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1 , 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		// 1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,
-	}
-	l, c := readPacket(bits, 0)
+	bits := parseMessage(getPuzzleInput("16_in.txt"))
+	fmt.Println(bits)
 
-	fmt.Println(l, c)
+	packet, _ := readPacket(bits, 0)
+	// fmt.Printf("%+v\n", packets)
+	partOne := packetArithmeticsVersions(packet)
+
+	fmt.Printf("Part 1: -> %d\n", partOne)
 }
 
+func packetArithmeticsValues(packet interface{}) int64 {
+	var values int64
 
+	switch interf := packet.(type) {
+	case Literal:
+		values += interf.Version
+	case Operator:
+		values += interf.Version
+
+		for _, subpacket := range interf.Packets {
+			values += packetArithmeticsVersions(subpacket)
+		}
+	}
+
+	return values
+}
+
+func packetArithmeticsVersions(packet interface{}) int64 {
+	var sumVersions int64
+
+	switch interf := packet.(type) {
+	case Literal:
+		sumVersions += interf.Version
+	case Operator:
+		sumVersions += interf.Version
+
+		for _, subpacket := range interf.Packets {
+			sumVersions += packetArithmeticsVersions(subpacket)
+		}
+	}
+
+	return sumVersions
+	
+}
 
 type Literal struct {
 	Version int64
@@ -33,20 +75,25 @@ type Operator struct {
 	Packets []interface{}
 }
 
-
 // Read packet
 func readPacket(data []byte, startpos int) (l interface{}, c int) {
+	pos := startpos
+
 	// Get version
 	version, count := readBits(data, startpos, 3)
-	pos := startpos + count
+	pos += count
+	fmt.Println("Version:", version)
 
 	// Get typeID
 	typeID, count := readBits(data, pos, 3)
 	pos += count
+	fmt.Println("TypeID:", typeID)
+
 
 	// Get the value of the packet
 	switch typeID {
 	case 4:
+		fmt.Println("Entering typeID = 4 case")
 		value, count := readNumber(data, pos)
 		pos += count
 
@@ -57,8 +104,11 @@ func readPacket(data []byte, startpos int) (l interface{}, c int) {
 		}, pos - startpos
 
 	default: // Other typeID than 4
+		fmt.Println("Entering typeID = not 4 case")
+
 		lengthID, count := readBits(data, pos, 1)
 		pos += count
+		fmt.Println("length ID =", lengthID)
 
 		op := Operator{
 			Version: version,
@@ -68,16 +118,20 @@ func readPacket(data []byte, startpos int) (l interface{}, c int) {
 		}
 
 		if lengthID == 0 {
-			length, _ := readBits(data, pos, 15)
+			length, count := readBits(data, pos, 15)
+			pos += count
+			fmt.Println("Entering length ID = 0. Length =", length)
+			
 			op.Length = length
 
 			subpacketPosStart := pos
 
-			for int64(pos - subpacketPosStart) < op.Length {
+			for int64(pos - subpacketPosStart) < length {
 				Count++
 				fmt.Println("Entering lengthID=0 loop, iteration number", Count)
-				fmt.Printf("Pos: %d, subpacketPosStart: %d, op.length: %d\n", pos, subpacketPosStart, op.Length)
-				packet, count := readNumber(data, pos)
+				fmt.Printf("Pos: %d, subpacketPosStart: %d, length: %d, op.length: %d\n", pos, subpacketPosStart, length, op.Length)
+				
+				packet, count := readPacket(data, pos)
 				pos += count
 
 				op.Packets = append(op.Packets, packet)
@@ -89,21 +143,18 @@ func readPacket(data []byte, startpos int) (l interface{}, c int) {
 		} else { // lengthID = 1
 			length, count := readBits(data, pos, 11)
 			op.Length = length
+			fmt.Println("Entering length ID = 1. Length =", length)
+
 			// fmt.Println(length)
 			pos += count
 
-			op := Operator{
-				Version: version,
-				TypeID: typeID,
-				LengthID: lengthID,
-				Length: length,
-				Packets: nil,
-			}
+			op.Length = length
+
 
 			for i := int64(0); i < length; i++ {
 				// fmt.Println(i, count)
 
-				fmt.Println("Entering readPacket for", i, "time")
+				fmt.Println("Length ID 1 - iteration number", i)
 				packet, count := readPacket(data, pos)
 				pos += count
 
@@ -111,10 +162,8 @@ func readPacket(data []byte, startpos int) (l interface{}, c int) {
 			}
 
 			return op, pos - startpos
-
 		}
 	}
-
 }
 
 func readNumber(data []byte, startpos int) (num int64, count int) {
@@ -135,9 +184,7 @@ func readNumber(data []byte, startpos int) (num int64, count int) {
 	}
 
 	return num, count
-
 }
-
 
 func readBits(data []byte, startpos, count int) (int64, int) {
 	var out int64
@@ -152,4 +199,45 @@ func readBits(data []byte, startpos, count int) (int64, int) {
 	}
 
 	return out, count
+}
+
+func parseMessage(data string) []byte {
+	fmt.Println(data)
+	message := ""
+	bits := make([]byte, 0)
+	for _, byte := range string(data) {
+		i, err := strconv.ParseUint(string(byte), 16, 8)
+    	if err != nil {
+        	fmt.Printf("%s", err)
+    	}
+		// fmt.Printf("%04b\n", i)
+
+		message += fmt.Sprintf("%04b", i)
+	}
+
+	for _, bit := range message {
+		newBit, err := strconv.ParseUint(string(bit), 16, 8); if err != nil { panic(err) }
+		bits = append(bits, byte(newBit))
+	}
+
+	return bits
+}
+
+func getPuzzleInput(filename string) string {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(bytes)
+
+	message := ""
+
+	for _, num := range string(bytes) {
+		message += string(num)
+	}
+
+	fmt.Println(message)
+	
+	return message
 }
