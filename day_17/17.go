aca196c7f287dec4b7bba148ef721e6c1908df1b
0x5ubt13 <5ubt13@protonmail.com>
2022-05-22 20:19:00 +0100

    17 completed!

--- day_17/17.go
+++ day_17/17.go
@@ -5,65 +5,69 @@ import (
 )
 
 func main() {
-	target := createTargetArea(88, 125, -103, -157)
-	target.test()
+	// target := createTargetArea(20, 30, -10, -5) // Test input
+	target := createTargetArea(88, 125, -157, -103) // Puzzle input
+	// target.test()
 
-	finalY := aim(target)
-	fmt.Println("Y =", finalY)
+	targetHitsCoordinates := autoAim(target)
+	// fmt.Printf("%+v\n", targetHitsCoordinates)
 
+	part1 := 0
+	for _, coord := range targetHitsCoordinates {
+		if coord.maxY > part1 {
+			part1 = coord.maxY
+		}
+	}
+	fmt.Printf("Part 1 -> %d\n", part1)
+ 	fmt.Printf("Part 2 -> %d\n", len(targetHitsCoordinates))
 }
 
-func aim(target *Target) int {
-	velocityY := -20
-	velocityX := 50
-	i := 0
+func autoAim(target *Target) ([]Coord) {
+	targetHitsCoordinates := make([]Coord, 0)
 
-	for velocityX > 0 {
-		try, steps := shootProbe(velocityY, velocityX, target)
-		if try == true {
-			fmt.Println("HIT!", try, steps, velocityY)
-			return velocityY
-		}
-
-		velocityY--
-		if i % 2 == 0 {
-			velocityX++
-		}
-	} 
-
-	return 0
-}
-
-func shootProbe(y, x int, target *Target) (bool, int) {
-	// Aceleration loses x-1 and x-1 per step. X can't be -1.
-	step := 0
-	for {
-		if x < 0 {
-			return false, step
-		}
-
-		if x > target.X["Max"]  {
-			return false, step
-		}
-
-		if y < target.Y["Max"] {
-			return false, step
-		}
-
-		fmt.Printf("Step %d, x = %d, y = %d ", step, x, y)
-		// Target hit
-		if x > target.X["Min"] && x < target.X["Max"] {
-			if y > target.Y["Min"] && y < target.Y["Max"] {
-				fmt.Println("HIT!")
-				return true, step
+	for x := 0; x < -target.Y["Min"]; x++ {
+		for y := target.Y["Min"]; y < -target.Y["Min"]; y++ {
+			hit, maxY := shootProbe(x, y, target)
+			if hit == true {
+				targetHitsCoordinates = append(targetHitsCoordinates, Coord{x, y, maxY})
 			}
+		}
+	}
+	
+	return targetHitsCoordinates
+}
+
+func shootProbe(x, y int, target *Target) (bool, int) {
+	// Aceleration loses x-1 and x-1 per step. X can't be -1.
+	maxY := 0
+	probeCoords := Coord{0,0,0}
+	for {
+		probeCoords.X += x
+		probeCoords.Y += y
+		if probeCoords.Y > maxY {
+			maxY = probeCoords.Y
+		}
+		// fmt.Printf("Step %d, speedX = %d, speedY = %d\nProbeX = %d, ProbeY = %d\n", step, x, y, probeCoords.X, probeCoords.Y)
+
+		// Miss returns false, hit returns true
+		if x > target.X["Max"]  {
+			return false, 0
+		}
+
+		if y < target.Y["Min"] {
+			return false, 0
+		}
+
+		// Target hit
+		if probeCoords.X >= target.X["Min"] && probeCoords.X <= target.X["Max"] && probeCoords.Y >= target.Y["Min"] && probeCoords.Y <= target.Y["Max"] {
+			return true, maxY
 		} 
 
 		// End of step
-		step++
-		
+		if x > 0 {
+			x--
+		}
 		y--
-		fmt.Printf("Miss.\n")
 	}
 } 
 
@@ -94,7 +98,7 @@ func createTargetArea(minX, maxX, minY, maxY int) *Target {
 	target.Area["Y"] = make([]int, 0)
 	target.Area["X"] = make([]int, 0)
 
-	for y := minY; y > maxY ; y-- {
+	for y := minY; y < maxY ; y++ {
 		target.Area["Y"] = append(target.Area["Y"], y)
 	}
 
@@ -103,7 +107,10 @@ func createTargetArea(minX, maxX, minY, maxY int) *Target {
 	}
 
 	return target
+}
 
+type Coord struct {
+	X, Y, maxY int
 }
 
 type Target struct {
