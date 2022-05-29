b4569af3d2218e53d1042ec75fc17a993660451b
0x5ubt13 <5ubt13@protonmail.com>
2022-05-30 00:01:43 +0100

    adding extra helper function

--- day_18/18.go
+++ day_18/18.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
+	"math"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,8 +14,8 @@ func main() {
 }
 
 func selector(lines []string) {
-	var finalNumber string
 	// Select between addition or reduction (exploding or splitting)
+	var finalNumber string
 
 	// loop over the new numbers
 	for i, nextNumber := range lines {
@@ -34,6 +35,8 @@ func selector(lines []string) {
 
 		// Repeat
 	}
+	// finalNumber = lines[0]
+	finalNumber = reduce(finalNumber)
 
 	fmt.Println(finalNumber)
 }
@@ -44,26 +47,46 @@ func add(finalNumber, nextNumber string) string {
 
 func reduce(finalNumber string) string {
 	// Loop through the number until no more reductions can be done
+
+	// Add a pointer to keep track of the position
+	// var pointer int = len(finalNumber)
+
 	for {
 		var clear1, clear2 bool
 
 		// First, check for nested pairs to explode. Explode pair if pair is nested inside 4 pairs
 		nestedLocationLeft, nestedLocationRight := lookForNestedPairs(finalNumber)
+		// pointer = nestedLocationRight
 		if nestedLocationLeft == 0 {
 			clear1 = true
 		} else {
-			// You are here
+			fmt.Println(finalNumber)
 			finalNumber = explode(finalNumber, nestedLocationLeft, nestedLocationRight)
 		}
 
-		break
+		// if clear1 == true {
+		// 	break // debug break
+		// }
+
 
 		// Second, check for splits. Split if any number > 10
 		bigNumLocation := lookForBigNumbers(finalNumber)
 		if len(bigNumLocation) == 0 {
 			clear2 = true
+		} else {
+			// TODO: if found big num what
+			fmt.Println(finalNumber)
+			finalNumber = split(finalNumber, bigNumLocation)
+			// nestedLocationLeft, nestedLocationRight := lookForNestedPairs(finalNumber)
+			// // pointer = nestedLocationRight
+			// if nestedLocationLeft == 0 {
+			// 	clear1 = true
+			// } else {
+			// 	fmt.Println(finalNumber)
+			// 	finalNumber = explode(finalNumber, nestedLocationLeft, nestedLocationRight)
+			// }
 		}
-		// TODO: if found big num what
+		
 
 		if clear1 == true && clear2 == true {
 			break
@@ -76,6 +99,7 @@ func reduce(finalNumber string) string {
 func explode(finalNumber string, nestedLocationLeft, nestedLocationRight int) string {
 	// nested left and nested right are the locations of the numbers to "explode"
 	// nested left -1 is the left bracket, right + 1 the right one
+	fmt.Println(finalNumber, nestedLocationLeft, nestedLocationRight, "slice:", finalNumber[nestedLocationLeft:nestedLocationRight])
 
 	// Transform to nums to sum with the next numbers on the sides, if any
 	explodingLeft, err := strconv.Atoi(string(finalNumber[nestedLocationLeft:nestedLocationRight][1])); if err != nil{panic(err)}
@@ -101,22 +125,61 @@ func explode(finalNumber string, nestedLocationLeft, nestedLocationRight int) st
 	if len(checkNumbersLeft) == 0 {
 		explodedMiddle += "0"
 	} else {
-		// find the rightmost, convert to int and add exploding one
-
+		// if not adjacent, add a 0 in the exploded pair 
+		if explodedLeft[len(explodedLeft)-2] > 57 || explodedLeft[len(explodedLeft)-2] < 48 && explodedMiddle != "0" {
+			explodedMiddle += "0"
+		}
+		// find the rightmost, convert to int, add exploding one, convert back to str and sub in str
+		target, err := strconv.Atoi(string(explodedLeft[checkNumbersLeft[len(checkNumbersLeft)-1][0]])); if err != nil{panic(err)}
+		target += explodingLeft
+		explodedLeft = explodedLeft[:checkNumbersLeft[len(checkNumbersLeft)-1][0]] + strconv.Itoa(target) + explodedLeft[checkNumbersLeft[len(checkNumbersLeft)-1][0]+1:]
 	}
 
 	checkNumbersRight := lookForNumbers(explodedRight)
 		// If no number right, plant a 0
 		if len(checkNumbersRight) == 0 {
 			explodedMiddle += "0"
+		} else {
+			// if not adjacent, add a 0 in the exploded pair 
+			fmt.Println(explodedRight[0])
+			if explodedRight[0] > 57 || explodedRight[0] < 48 && explodedMiddle != "0"{
+				explodedMiddle += "0"
+			}
+			// find the leftmost, convert to int, add exploding one, convert back to string and substitute in the string
+			target, err := strconv.Atoi(string(explodedRight[checkNumbersRight[0][0]])); if err != nil{panic(err)}
+			target += explodingRight
+			explodedRight = explodedRight[:checkNumbersRight[0][0]] + strconv.Itoa(target) + explodedRight[checkNumbersRight[0][0]+1:]
 		}
 
-	explodedNumber := explodedLeft + " " + explodedMiddle + " " + explodedRight 
+	fmt.Println(explodedLeft + "/" + explodedMiddle + "/" + explodedRight)
+	explodedNumber := explodedLeft + explodedMiddle + explodedRight 
+ 
 	return explodedNumber
 }
 
-func split() {
+func split(finalNumber string, bigNumLocation []int) string {
+	// bigNumLocation indicates slice to cut
+	splittingLeft := finalNumber[:bigNumLocation[0]]
+	splittingRight := finalNumber[bigNumLocation[1]:]
+	splittingNum, err := strconv.Atoi(finalNumber[bigNumLocation[0]:bigNumLocation[1]]); if err!=nil{panic(err)}
+	fmt.Println("Extracted:", splittingNum)
+	fmt.Println(splittingLeft + "| |" + splittingRight)
+	var leftSplit float64
+	if splittingNum % 2 != 0 {
+		leftSplit = math.Trunc(float64(splittingNum) / 2)
+		rightSplit := leftSplit + 1
+	
+		return fmt.Sprintf("%v[%v,%v]%v", splittingLeft, int64(leftSplit), int64(rightSplit), splittingRight)
+	} else {
+		leftSplit = float64(splittingNum) / 2 
+		
+		return fmt.Sprintf("%v[%v,%v]%v", splittingLeft, int64(leftSplit), int64(leftSplit), splittingRight) 
+	}
+}
 
+func extractNestedSlice() {
+	// work on the level of the nested pair to avoid crashing the program
+	
 }
 
 func lookForNumbers(snailfishNumber string) [][]int {
@@ -154,7 +217,13 @@ func lookForNestedPairs(snailfishNumber string) (int, int) {
 		if leftBracket == 5 {
 			// Returns the location of the left bracket of the pair needing to explode
 			// and the location of the comma afterwards
-			return i, i+5
+			for {
+				if snailfishNumber[i+1] == '[' || snailfishNumber[i+1] == ',' || snailfishNumber[i+3] == '[' || snailfishNumber[i+3] == ',' {
+					i++
+				} else {
+					return i, i+5
+				}
+			}
 		}
 
 		if ch == ']' {
